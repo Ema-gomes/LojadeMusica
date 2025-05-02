16e1f1dc5f790c6c8d5414da9941ff1a57b8ce75
beatrizsimoeess <abass@iscte-iul.pt>
2025-05-02 11:00:50 +0100

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,3 +1,25 @@
-kjfuhwiuhfelwei
-fgwsleudfhc
-djuvgs
+import React from "react";
+import './Header.css';
+import { useNavigate } from 'react-router-dom';
+
+function Header (){
+const navigate = useNavigate();
+
+return (
+<header className="header">
+
+  <div className="left-section">
+  </div>
+
+  <div className="logo">LALALAALAL</div>
+
+  < nav className="right-section">
+  <div className="account" onClick={() => navigate('/account')}>
+  <p> 👤 Conta </p>
+        </div>
+        </nav>
+        </header>
+);
+}
+
+export default Header;
